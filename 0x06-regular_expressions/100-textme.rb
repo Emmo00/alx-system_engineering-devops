47c0b07264784beb044039227ef278e0e0238f69
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-05-04 12:50:33 -0700

    he

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/.*\[from:(?<SENDER\.>\w*)\]\s\[to:(?<RECEIVER>.*)[0-9]\]\s\[flags:(?<FLAGS>.*)\]\s\[msg:.*/).join(",")
+puts ARGV[0].scan(/(\[from:(?<SENDER>\w+)\]\s\[to:(?<RECEIVER>.+)[0-9]\]\s\[flags:(?<FLAGS>.+)\])\s\[msg:/).join(",")
