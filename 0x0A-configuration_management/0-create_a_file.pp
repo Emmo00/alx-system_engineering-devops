a2939763a15ba386f02be6ca84793ee082c9b4f0
Emmanuel Nwafor <88244468+Emmo00@users.noreply.github.com>
2023-06-23 22:28:16 +0100

    unencap

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
 # create a file in /tmp
-class filetest {
 file { '/tmp/school':
   ensure  => present,
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
   content  => 'I love Puppet',
-   }
 }
