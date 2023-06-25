af3e091324d2c0a3909cba1c00fd7db249e6ae40
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-06-25 18:38:19 +0100

    puppet ssh config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,22 @@
+file { '~/.ssh':
+    ensure  => directory,
+    owner   => 'slave',
+    group   => 'slave',
+    mode    => '0700',
+  }
+
+  file { '~/.ssh/school':
+    ensure  => present,
+    owner   => 'slave',
+    group   => 'slave',
+    mode    => '0600',
+    source  => 'puppet:///modules/ssh_slave_config/school',
+  }
+
+  file { '/etc/ssh/ssh_config':
+    ensure  => present,
+    owner   => 'slave',
+    group   => 'slave',
+    mode    => '0600',
+    content => "Host *\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication no\n",
+  }
