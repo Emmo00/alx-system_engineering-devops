89ad71622d397d67a727b02531ec8d5545cf60a8
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-06-27 20:10:31 +0100

    puppet_huh

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,19 @@
+# configure nginx server
+package { 'nginx':
+  ensure => installed,
+}
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
+file { 'add permanent redirect'
+  path   => '/etc/nginx/sites-available/default'
+  line   => "server_name _;\nrewrite ^/redirect_me / permanent;"
+  match  => '^server\s*{'
+  ensure => present,
+}
+file { '/var/www/html/index.html'
+  path    => '/var/www/html/index.html'
+  ensure  => present
+  content => 'Hello World!'
+}
