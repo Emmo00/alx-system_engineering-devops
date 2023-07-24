76f33a09981ba28ee767565fffaa13995d636122
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-07-24 20:55:51 +0100

    all to dict

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""export data as csv"""
+import json
+import requests
+
+if __name__ == '__main__':
+    users_url = f'https://jsonplaceholder.typicode.com/users'
+    users = requests.get(users_url).json()
+
+    data = {}
+    for user in users:
+        user_id = user['id']
+        tasks_url = f'https://jsonplaceholder.typicode.com/todos'\
+                    f'?userId={user_id}'
+        print(tasks_url)
+        tasks = requests.get(tasks_url).json()
+        data[f'{user_id}'] = [{'username': user['username'],
+                               'task': task['title'],
+                               'completed': task['completed']}
+                              for task in tasks]
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(data, f)
