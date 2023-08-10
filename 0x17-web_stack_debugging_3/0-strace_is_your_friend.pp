b42cae0b3727ba0412d0e1856b29b1ab5c4985d4
Emmanuel Nwafor <88244468+Emmo00@users.noreply.github.com>
2023-08-10 19:09:13 +0100

    Escape

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # correct require statement
 
 exec {'correct import extension':
-	command => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+	command => "sudo sed -i 's/\.phpp/\.php/g' /var/www/html/wp-settings.php",
 	path    => '/usr/local/bin/:/bin/',
 }
