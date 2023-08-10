72f595430985e5f1ef2a1aa38af68e9b7dce2be3
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-08-10 12:45:48 -0700

    s

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # correct require statement
 
 exec {'correct import extension':
-  command => "sudo sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
   path    => '/usr/local/bin/:/bin/',
-  return  => [0,1],
 }
