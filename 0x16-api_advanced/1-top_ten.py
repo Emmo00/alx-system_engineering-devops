9030e453f30294231e25b23e67ea67885bc35369
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-08-10 07:51:00 -0700

    top ten and dont include vi tmp files

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defines the top ten function"""
+import requests
+
+
+def top_ten(subreddit):
+    """get the titles of top ten posts of a subreddit"""
+    url = f'https://www.reddit.com/r/{subreddit}/top.json'
+    res = requests.get(url, params={'limit': 10}, headers={'User-Agent': '1-top_ten'}).json()
+    error = res.get('error')
+    if error:
+        print("None")
+        return
+    for post in res['data']['children']:
+        print(post['data']['title'])
