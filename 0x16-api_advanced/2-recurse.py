a2bf0abea9bb37dc1f48fc29ae663c32f689a4f5
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-08-10 08:40:00 -0700

    no redirects

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,10 @@ def recurse(subreddit, hot_list=[], after=None):
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     params = {'after': after} if after else {}
     res = requests.get(url, allow_redirects=False, params=params,
-                       headers={'User-Agent': '2-recurse'}).json()
+                       headers={'User-Agent': '2-recurse'})
+    if res.status_code == 302:
+        return None
+    res = res.json()
     err = res.get('error')
     if err or not res.get('data') or not res.get('data').get('children'):
         return None
