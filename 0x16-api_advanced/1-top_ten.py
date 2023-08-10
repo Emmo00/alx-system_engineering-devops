2bebe02e8a19de4ae1474c1da13f67114557acb4
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-08-10 07:56:40 -0700

    thou shall not follow redirects

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """get the titles of top ten posts of a subreddit"""
     url = f'https://www.reddit.com/r/{subreddit}/top.json'
-    res = requests.get(url, params={'limit': 10},
+    res = requests.get(url, params={'limit': 10}, allow_redirects=False,
                        headers={'User-Agent': '1-top_ten'}).json()
     error = res.get('error')
     if error:
