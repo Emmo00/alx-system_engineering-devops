10388a0017b14cfa8b772a0410fde6418ebc19fa
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-08-10 08:08:17 -0700

    hot not top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,11 +5,11 @@ import requests
 
 def top_ten(subreddit):
     """get the titles of top ten posts of a subreddit"""
-    url = f'https://www.reddit.com/r/{subreddit}/top.json'
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     res = requests.get(url, params={'limit': 10}, allow_redirects=False,
                        headers={'User-Agent': '1-top_ten'}).json()
-    error = res.get('error')
-    if error:
+    err = res.get('error')
+    if err or not res.get('data') or not res.get('data').get('children'):
         print("None")
         return
     for post in res['data']['children']:
