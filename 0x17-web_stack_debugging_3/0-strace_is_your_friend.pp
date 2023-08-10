3243b45be7d3275c4d5651bd929e3382372bedf1
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-08-10 10:34:30 -0700

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# correct require statement
+exec { "correct import extension":
+	command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+	path    => '/usr/local/bin/:/bin/',
+}
